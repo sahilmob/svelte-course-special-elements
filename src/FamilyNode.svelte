95aaf4084194f54111dd3052053e70a2e4f00fa8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-23 19:15:33 +0300

    style children

--- src/FamilyNode.svelte
+++ src/FamilyNode.svelte
@@ -2,10 +2,18 @@
   export let member;
 </script>
 
-<h1>{member.name}</h1>
+<style>
+  div {
+    margin: 1rem;
+  }
+</style>
 
-{#if member.isParent}
-  {#each member.children as child}
-    <svelte:self member={child} />
-  {/each}
-{/if}
+<div>
+  <h1>{member.name}</h1>
+  {#if member.isParent}
+    {#each member.children as child}
+      <svelte:self member={child} />
+    {/each}
+  {/if}
+
+</div>
