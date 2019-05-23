e82a9c561db0f2455f93b6825764eabc1b9c8dc7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-23 18:53:52 +0300

    dynamic component demo

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Product from "./Product.svelte";
+  import CartItem from "./CartItem.svelte";
+
+  let renderedComponent = CartItem;
+
+  function toggle() {
+    if (renderedComponent === CartItem) {
+      return (renderedComponent = Product);
+    }
+    renderedComponent = CartItem;
+  }
+</script>
+
+<button on:click={toggle}>Toggle</button>
+
+<svelte:component this={renderedComponent} title={renderedComponent === CartItem ? "Another product" : "Test product"} id="p1" />
